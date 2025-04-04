8c2600a1e8542dac96474360bbef79fd37248ba1
Tomás Ossa <ossatomas2@gmail.com>
2025-04-04 19:30:24 -0300

    test: update FormSectionCompletionAssistant.test.ts

--- src/FormCompletionAssistant/FormSectionCompletionAssistant.test.ts
+++ src/FormCompletionAssistant/FormSectionCompletionAssistant.test.ts
@@ -13,6 +13,7 @@ describe("FormSectionCompletionAssistant", () => {
     expect(assistant.hasFailedAssertions()).toBe(false);
     expect(assistant.doesNotHaveFailedAssertions()).toBe(true);
     expect(assistant.handles(Assertion.for(1, "AID.1", () => true, "Description 1"))).toBe(false);
+    expect(assistant.failedAssertionsDescriptions()).toEqual([]);
   });
 
   it("should handle an assertion identified with a stored id", () => {
@@ -31,5 +32,16 @@ describe("FormSectionCompletionAssistant", () => {
 
     expect(assistant.handles(anAssertion)).toBe(true);
     expect(assistant.handles(anotherAssertion)).toBe(true);
+  });
+
+  it("should accept failed assertions", () => {
+    const assistant = TestObjectsBucket.createTestModelAssistant();
+
+    assistant.addFailedAssertion(TestObjectsBucket.defaultFailingAssertion());
+
+    expect(assistant.hasOnlyOneAssertionFailedIdentifiedAs(TestObjectsBucket.defaultFailingAssertionAID)).toBe(true);
+    expect(assistant.hasFailedAssertions()).toBe(true);
+    expect(assistant.doesNotHaveFailedAssertions()).toBe(false);
+    expect(assistant.failedAssertionsDescriptions()).toEqual([TestObjectsBucket.defaultFailingAssertionDescription]);
   });
 });
