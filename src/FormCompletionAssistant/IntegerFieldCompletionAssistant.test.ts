0f642c6e1d113c3cb0001b74a66e5a428b6e8900
Tomás Ossa <ossatomas2@gmail.com>
2025-04-05 11:30:03 -0300

    test: update IntegerFieldCompletionAssistant.test.ts

--- src/FormCompletionAssistant/IntegerFieldCompletionAssistant.test.ts
+++ src/FormCompletionAssistant/IntegerFieldCompletionAssistant.test.ts
@@ -3,14 +3,14 @@ import { IntegerFieldCompletionAssistant } from "./IntegerFieldCompletionAssista
 import { FormCompletionAssistant } from "./FormCompletionAssistant";
 
 describe("IntegerFieldCompletionAssistant", () => {
-  it("should allow positive integers as strings", (done) => {
+  it("should allow non-negative integers as strings", (done) => {
     const assistant = IntegerFieldCompletionAssistant.forTopLevel("");
-    assistant.setInnerModel("1");
+    assistant.setInnerModel("0");
 
     assistant.withCreatedModelDo(
       (model) => {
-        expect(assistant.getInnerModel()).toBe("1");
-        expect(model).toBe(1);
+        expect(assistant.getInnerModel()).toBe("0");
+        expect(model).toBe(0);
         done();
       },
       () => done("Should not be invalid")
@@ -58,5 +58,52 @@ describe("IntegerFieldCompletionAssistant", () => {
         done();
       }
     );
+  });
+
+  it("should be invalid if the inner model is not a number", (done) => {
+    const assertionId = "notANumberAssertionAID";
+    const assistant = IntegerFieldCompletionAssistant.forTopLevel(assertionId);
+    assistant.setInnerModel("not a number");
+
+    assistant.withCreatedModelDo(
+      () => done("Should be invalid"),
+      () => {
+        expect(FormCompletionAssistant.isInvalidModel(assistant.getModel())).toBe(true);
+        expect(assistant.hasOnlyOneAssertionFailedIdentifiedAs(assertionId)).toBe(true);
+        done();
+      }
+    );
+  });
+
+  it("should be invalid if the inner model is not an integer", (done) => {
+    const assertionId = "notAnIntegerAssertionAID";
+    const assistant = IntegerFieldCompletionAssistant.forTopLevel(assertionId);
+    assistant.setInnerModel("0.1");
+
+    assistant.withCreatedModelDo(
+      () => done("Should be invalid"),
+      () => {
+        expect(FormCompletionAssistant.isInvalidModel(assistant.getModel())).toBe(true);
+        expect(assistant.hasOnlyOneAssertionFailedIdentifiedAs(assertionId)).toBe(true);
+        done();
+      }
+    );
+  });
+
+  describe("inner assistant", () => {
+    it("should allow setting its value from an integer", (done) => {
+      const assistant = IntegerFieldCompletionAssistant.forTopLevel("");
+
+      assistant.innerAssistant().setModelFrom(1);
+
+      assistant.withCreatedModelDo(
+        (model) => {
+          expect(assistant.getInnerModel()).toBe("1");
+          expect(model).toBe(1);
+          done();
+        },
+        () => done("Should not be invalid")
+      );
+    });
   });
 });
