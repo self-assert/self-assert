81a5f6220ffa11ae5ddea5f11bfc57dcfa5607f1
Tomás Ossa <ossatomas2@gmail.com>
2025-04-17 16:03:03 -0300

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-confusing-void-expression": ["off"],
       "@typescript-eslint/no-extraneous-class": ["off"],
+      "@typescript-eslint/restrict-template-expressions": ["off"],
     },
   }
 );
