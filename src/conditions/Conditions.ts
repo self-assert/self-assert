2fc7014456216d889dcf0f227aa3bc3876f13ab7
Tomás Ossa <ossatomas2@gmail.com>
2025-04-18 12:33:21 -0300

    Refactor with ConditionsCompositions

--- src/conditions/Conditions.ts
+++ src/conditions/Conditions.ts
@@ -1,17 +1,35 @@
+type Predicate<ValueType> = (value: ValueType) => boolean;
+
+const ConditionsCompositions = {
+  or:
+    <ValueType>(...conditions: Predicate<ValueType>[]): Predicate<ValueType> =>
+    (value) =>
+      conditions.some((condition) => condition(value)),
+  and:
+    <ValueType>(...conditions: Predicate<ValueType>[]): Predicate<ValueType> =>
+    (value) =>
+      conditions.every((condition) => condition(value)),
+  not:
+    <ValueType>(condition: Predicate<ValueType>): Predicate<ValueType> =>
+    (value) =>
+      !condition(value),
+};
+
 export const Conditions = {
   hold: () => true,
   fail: () => false,
-
-  greaterThan: (aNumber: number) => (value: number) => value > aNumber,
-  lessThan(aNumber: number) {
-    return (value: number) => !this.greaterThan(aNumber)(value) && value !== aNumber;
-  },
-
+  ...ConditionsCompositions,
+  greaterThan:
+    (aNumber: number): Predicate<number> =>
+    (value) =>
+      value > aNumber,
   greaterThanOrEqual(aNumber: number) {
-    return (value: number) => this.greaterThan(aNumber)(value) || value === aNumber;
+    return this.or(this.greaterThan(aNumber), (value: number) => value === aNumber);
   },
-
+  lessThan(aNumber: number) {
+    return this.not(this.greaterThanOrEqual(aNumber));
+  },
   lessThanOrEqual(aNumber: number) {
-    return (value: number) => this.lessThan(aNumber)(value) || value === aNumber;
+    return this.not(this.greaterThan(aNumber));
   },
 };
