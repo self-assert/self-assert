7d0d9f24b52c5e597d1295ff627a3b30d5a1093c
Tomás Ossa <ossatomas2@gmail.com>
2025-04-18 16:08:39 -0300

    Add predicates about number of elements in arrays and strings

--- src/conditions/lists/ListsConditions.test.ts
+++ src/conditions/lists/ListsConditions.test.ts
@@ -18,4 +18,61 @@ describe("Lists", () => {
     expect(isNotEmpty("d")).toBe(true);
     expect(isNotEmpty([0])).toBe(true);
   });
+
+  it("should provide a condition that holds when the list includes an element", () => {
+    const includes = Conditions.includes("a");
+    expect(includes([] as string[])).toBe(false);
+    expect(includes(["a"])).toBe(true);
+    expect(includes(["b"])).toBe(false);
+    expect(includes(["b", "a"])).toBe(true);
+    expect(includes("bbbabbb")).toBe(true);
+  });
+
+  it("should provide a condition that holds when the list does not include an element", () => {
+    const doesNotInclude = Conditions.doesNotInclude("a");
+    expect(doesNotInclude([] as string[])).toBe(true);
+    expect(doesNotInclude(["a"])).toBe(false);
+    expect(doesNotInclude(["b"])).toBe(true);
+    expect(doesNotInclude(["b", "a"])).toBe(false);
+    expect(doesNotInclude("bbbabbb")).toBe(false);
+  });
+
+  it("should provide a condition that holds when the list has more than a number of elements", () => {
+    const hasMoreThan = Conditions.hasMoreThan(2);
+    expect(hasMoreThan([] as string[])).toBe(false);
+    expect(hasMoreThan(["a"])).toBe(false);
+    expect(hasMoreThan(["a", "b"])).toBe(false);
+    expect(hasMoreThan(["a", "b", "c"])).toBe(true);
+    expect(hasMoreThan("abcd")).toBe(true);
+  });
+
+  it("should provide a condition that holds when the list has less than a number of elements", () => {
+    const hasLessThan = Conditions.hasLessThan(2);
+    expect(hasLessThan([] as string[])).toBe(true);
+    expect(hasLessThan(["a"])).toBe(true);
+    expect(hasLessThan("a")).toBe(true);
+    expect(hasLessThan(["a", "b"])).toBe(false);
+    expect(hasLessThan(["a", "b", "c"])).toBe(false);
+    expect(hasLessThan("abcd")).toBe(false);
+  });
+
+  it("should provide a condition that holds when the list has up to a number of elements", () => {
+    const hasUpTo = Conditions.hasUpTo(2);
+    expect(hasUpTo([] as string[])).toBe(true);
+    expect(hasUpTo(["a"])).toBe(true);
+    expect(hasUpTo("a")).toBe(true);
+    expect(hasUpTo(["a", "b"])).toBe(true);
+    expect(hasUpTo(["a", "b", "c"])).toBe(false);
+    expect(hasUpTo("abcd")).toBe(false);
+  });
+
+  it("should provide a condition that holds when the list has exactly a number of elements", () => {
+    const hasExactly = Conditions.hasExactly(2);
+    expect(hasExactly([] as string[])).toBe(false);
+    expect(hasExactly(["a"])).toBe(false);
+    expect(hasExactly("a")).toBe(false);
+    expect(hasExactly(["a", "b"])).toBe(true);
+    expect(hasExactly(["a", "b", "c"])).toBe(false);
+    expect(hasExactly("abcd")).toBe(false);
+  });
 });
--- src/conditions/lists/ListsConditions.ts
+++ src/conditions/lists/ListsConditions.ts
@@ -1,11 +1,27 @@
 import { LogicalConditions } from "../LogicalConditions";
 
-const { identical, not } = LogicalConditions;
+const { identical, not, and } = LogicalConditions;
 
 import type { Predicate } from "../Conditions";
+import { NumbersConditions } from "../numbers/NumbersConditions";
 
 export class ListsConditions {
-  static isEmpty: Predicate<ArrayLike<unknown>> = (list) => identical(0)(list.length);
+  static hasExactly = (aNumber: number) => (list: ArrayLike<unknown>) => identical(aNumber)(list.length);
+
+  static isEmpty: Predicate<ArrayLike<unknown>> = this.hasExactly(0);
 
   static isNotEmpty = not(this.isEmpty);
+
+  static hasMoreThan = (aNumber: number) => (list: ArrayLike<unknown>) =>
+    NumbersConditions.greaterThan(aNumber)(list.length);
+
+  static hasUpTo = (aNumber: number) => not(this.hasMoreThan(aNumber));
+
+  static hasLessThan = (aNumber: number) => and(this.hasUpTo(aNumber), not(this.hasExactly(aNumber)));
+
+  static includes: <ElementType>(element: ElementType) => Predicate<{ includes: Predicate<ElementType> }> =
+    (element) => (list) =>
+      list.includes(element);
+
+  static doesNotInclude = <ElementType>(element: ElementType) => not(this.includes(element));
 }
