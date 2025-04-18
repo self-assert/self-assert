be92ec243a71105726106af0fc3d3afcfa41e2ce
Tomás Ossa <ossatomas2@gmail.com>
2025-04-18 12:20:41 -0300

    Add more conditions about numbers

--- src/conditions/Conditions.test.ts
+++ src/conditions/Conditions.test.ts
@@ -11,9 +11,20 @@ describe("Conditions", () => {
   });
 
   describe("Numbers", () => {
-    it("should provide a condition that holds when the number is greater than a value", () => {
+    it("should provide a condition that holds when the value is greater than a number", () => {
       expect(Conditions.greaterThan(0)(0.1)).toBe(true);
       expect(Conditions.greaterThan(0)(0)).toBe(false);
+    });
+
+    it("should provide a condition that holds when the value is less than a number", () => {
+      expect(Conditions.lessThan(0)(-0.1)).toBe(true);
+      expect(Conditions.lessThan(0)(0)).toBe(false);
+    });
+
+    it("should provide a condition that holds when the value is greater than or equal to a number", () => {
+      expect(Conditions.greaterThanOrEqual(0)(0.1)).toBe(true);
+      expect(Conditions.greaterThanOrEqual(0)(0)).toBe(true);
+      expect(Conditions.greaterThanOrEqual(0)(-0.1)).toBe(false);
     });
   });
 });
--- src/conditions/Conditions.ts
+++ src/conditions/Conditions.ts
@@ -3,4 +3,11 @@ export const Conditions = {
   fail: () => false,
 
   greaterThan: (aNumber: number) => (value: number) => value > aNumber,
+  lessThan(aNumber: number) {
+    return (value: number) => !this.greaterThan(aNumber)(value) && value !== aNumber;
+  },
+
+  greaterThanOrEqual(aNumber: number) {
+    return (value: number) => this.greaterThan(aNumber)(value) || value === aNumber;
+  },
 };
