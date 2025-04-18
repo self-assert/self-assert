0a4586e7471b7c6a6e65e1843048f0a2e0340bfe
Tomás Ossa <ossatomas2@gmail.com>
2025-04-18 18:01:17 -0300

    More conditions about lists

--- src/conditions/lists/ListsConditions.test.ts
+++ src/conditions/lists/ListsConditions.test.ts
@@ -38,41 +38,82 @@ describe("Lists", () => {
   });
 
   it("should provide a condition that holds when the list has more than a number of elements", () => {
-    const hasMoreThan = Conditions.hasMoreThan(2);
-    expect(hasMoreThan([] as string[])).toBe(false);
-    expect(hasMoreThan(["a"])).toBe(false);
-    expect(hasMoreThan(["a", "b"])).toBe(false);
-    expect(hasMoreThan(["a", "b", "c"])).toBe(true);
-    expect(hasMoreThan("abcd")).toBe(true);
+    const hasMoreThan1 = Conditions.hasMoreThan(1);
+    expect(hasMoreThan1([] as string[])).toBe(false);
+    expect(hasMoreThan1(["a"])).toBe(false);
+    expect(hasMoreThan1(["a", "b"])).toBe(true);
+    expect(hasMoreThan1(["a", "b", "c"])).toBe(true);
+    expect(hasMoreThan1("abcd")).toBe(true);
   });
 
   it("should provide a condition that holds when the list has less than a number of elements", () => {
-    const hasLessThan = Conditions.hasLessThan(2);
-    expect(hasLessThan([] as string[])).toBe(true);
-    expect(hasLessThan(["a"])).toBe(true);
-    expect(hasLessThan("a")).toBe(true);
-    expect(hasLessThan(["a", "b"])).toBe(false);
-    expect(hasLessThan(["a", "b", "c"])).toBe(false);
-    expect(hasLessThan("abcd")).toBe(false);
+    const hasLessThan3 = Conditions.hasLessThan(3);
+    expect(hasLessThan3([] as string[])).toBe(true);
+    expect(hasLessThan3(["a"])).toBe(true);
+    expect(hasLessThan3("a")).toBe(true);
+    expect(hasLessThan3(["a", "b"])).toBe(true);
+    expect(hasLessThan3(["a", "b", "c"])).toBe(false);
+    expect(hasLessThan3("abcd")).toBe(false);
   });
 
-  it("should provide a condition that holds when the list has up to a number of elements", () => {
-    const hasUpTo = Conditions.hasUpTo(2);
-    expect(hasUpTo([] as string[])).toBe(true);
-    expect(hasUpTo(["a"])).toBe(true);
-    expect(hasUpTo("a")).toBe(true);
-    expect(hasUpTo(["a", "b"])).toBe(true);
-    expect(hasUpTo(["a", "b", "c"])).toBe(false);
-    expect(hasUpTo("abcd")).toBe(false);
+  it("should provide a condition that holds when the list has at most a number of elements", () => {
+    const hasAtMost2 = Conditions.hasAtMost(2);
+    expect(hasAtMost2([] as string[])).toBe(true);
+    expect(hasAtMost2(["a"])).toBe(true);
+    expect(hasAtMost2("a")).toBe(true);
+    expect(hasAtMost2(["a", "b"])).toBe(true);
+    expect(hasAtMost2(["a", "b", "c"])).toBe(false);
+    expect(hasAtMost2("abcd")).toBe(false);
+  });
+
+  it("should provide a condition that holds when the list has at least a number of elements", () => {
+    const hasAtLeast3 = Conditions.hasAtLeast(3);
+    expect(hasAtLeast3([] as string[])).toBe(false);
+    expect(hasAtLeast3(["a"])).toBe(false);
+    expect(hasAtLeast3("a")).toBe(false);
+    expect(hasAtLeast3(["a", "b"])).toBe(false);
+    expect(hasAtLeast3(["a", "b", "c"])).toBe(true);
+    expect(hasAtLeast3("abcd")).toBe(true);
   });
 
   it("should provide a condition that holds when the list has exactly a number of elements", () => {
-    const hasExactly = Conditions.hasExactly(2);
-    expect(hasExactly([] as string[])).toBe(false);
-    expect(hasExactly(["a"])).toBe(false);
-    expect(hasExactly("a")).toBe(false);
-    expect(hasExactly(["a", "b"])).toBe(true);
-    expect(hasExactly(["a", "b", "c"])).toBe(false);
-    expect(hasExactly("abcd")).toBe(false);
+    const hasExactly2 = Conditions.hasExactly(2);
+    expect(hasExactly2([] as string[])).toBe(false);
+    expect(hasExactly2(["a"])).toBe(false);
+    expect(hasExactly2("a")).toBe(false);
+    expect(hasExactly2(["a", "b"])).toBe(true);
+    expect(hasExactly2(["a", "b", "c"])).toBe(false);
+    expect(hasExactly2("abcd")).toBe(false);
+  });
+
+  it("should provide a condition that holds when all elements of the list satisfy a condition", () => {
+    const allSatisfy = Conditions.allSatisfy(Conditions.isInteger);
+    expect(allSatisfy([] as number[])).toBe(true);
+    expect(allSatisfy([0])).toBe(true);
+    expect(allSatisfy([1, 2, 3])).toBe(true);
+    expect(allSatisfy([1, 2, "3", 4])).toBe(false);
+    expect(allSatisfy([1, 2, 3, 4.1])).toBe(false);
+
+    expect(Conditions.allSatisfy((str: string) => str === "a")("aaa")).toBe(true);
+  });
+
+  it("should provide a condition that holds when any element of the list satisfies a condition", () => {
+    const anySatisfy = Conditions.anySatisfy(Conditions.isInteger);
+    expect(anySatisfy([] as number[])).toBe(false);
+    expect(anySatisfy([0])).toBe(true);
+    expect(anySatisfy([1, 2, 3])).toBe(true);
+    expect(anySatisfy([1, 2, "3", 4])).toBe(true);
+    expect(anySatisfy([1, 2, 3, 4.1])).toBe(true);
+    expect(anySatisfy(["1", "2", "3", "4"])).toBe(false);
+  });
+
+  it("should provide a condition that holds when no element of the list satisfies a condition", () => {
+    const noneSatisfy = Conditions.noneSatisfy(Conditions.isInteger);
+    expect(noneSatisfy([] as number[])).toBe(true);
+    expect(noneSatisfy([0])).toBe(false);
+    expect(noneSatisfy([1, 2, 3])).toBe(false);
+    expect(noneSatisfy([1, 2, "3", 4])).toBe(false);
+    expect(noneSatisfy([1, 2, 3, 4.1])).toBe(false);
+    expect(noneSatisfy(["1", "2", "3", "4"])).toBe(true);
   });
 });
--- src/conditions/lists/ListsConditions.ts
+++ src/conditions/lists/ListsConditions.ts
@@ -1,9 +1,8 @@
 import { LogicalConditions } from "../LogicalConditions";
+import { NumbersConditions } from "../numbers/NumbersConditions";
+import type { Predicate } from "../Conditions";
 
 const { identical, not, and } = LogicalConditions;
-
-import type { Predicate } from "../Conditions";
-import { NumbersConditions } from "../numbers/NumbersConditions";
 
 export class ListsConditions {
   static hasExactly = (aNumber: number) => (list: ArrayLike<unknown>) => identical(aNumber)(list.length);
@@ -15,13 +14,39 @@ export class ListsConditions {
   static hasMoreThan = (aNumber: number) => (list: ArrayLike<unknown>) =>
     NumbersConditions.greaterThan(aNumber)(list.length);
 
-  static hasUpTo = (aNumber: number) => not(this.hasMoreThan(aNumber));
+  static hasAtMost = (aNumber: number) => not(this.hasMoreThan(aNumber));
 
-  static hasLessThan = (aNumber: number) => and(this.hasUpTo(aNumber), not(this.hasExactly(aNumber)));
+  static hasLessThan = (aNumber: number) => and(this.hasAtMost(aNumber), not(this.hasExactly(aNumber)));
+
+  static hasAtLeast = (aNumber: number) => not(this.hasLessThan(aNumber));
 
   static includes: <ElementType>(element: ElementType) => Predicate<{ includes: Predicate<ElementType> }> =
     (element) => (list) =>
       list.includes(element);
 
   static doesNotInclude = <ElementType>(element: ElementType) => not(this.includes(element));
+
+  static allSatisfy =
+    <ElementType>(predicate: Predicate<ElementType>) =>
+    (list: Iterable<ElementType>) => {
+      for (const element of list) {
+        if (!predicate(element)) {
+          return false;
+        }
+      }
+      return true;
+    };
+
+  static anySatisfy =
+    <ElementType>(predicate: Predicate<ElementType>) =>
+    (list: Iterable<ElementType>) => {
+      for (const element of list) {
+        if (predicate(element)) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+  static noneSatisfy = <ElementType>(predicate: Predicate<ElementType>) => not(this.anySatisfy(predicate));
 }
