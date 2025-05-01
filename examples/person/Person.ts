d2c74147fc982709d1e36e22f38c7aca410d321b
Tomás Ossa <ossatomas2@gmail.com>
2025-05-01 11:21:17 -0300

    docs(examples): update Person example

---
+++ examples/person/Person.ts
@@ -0,0 +1,36 @@
+import { Assertion, Requirements, Ruleset } from "self-assert";
+
+export class Person {
+  static readonly nameNotBlank = Assertion.requiring(
+    "name.notBlank",
+    "Name must not be blank",
+    Requirements.isNotBlank
+  );
+
+  static readonly agePositive = Assertion.requiring(
+    "age.positive",
+    "Age must be positive",
+    Requirements.isPositive
+  );
+
+  static named(name: string, age: number) {
+    Ruleset.ensureAll(
+      this.nameNotBlank.evaluateFor(name),
+      this.agePositive.evaluateFor(age)
+    );
+    return new this(name, age);
+  }
+
+  protected constructor(protected name: string, protected age: number) {}
+
+  getName() {
+    return this.name;
+  }
+
+  getAge() {
+    return this.age;
+  }
+}
+
+Person.named("John", 50);
+Person.named("   ", 0);
---
+++ examples/person/PersonAssistant.ts
@@ -0,0 +1,54 @@
+import {
+  FieldDraftAssistant,
+  IntegerDraftAssistant,
+  SectionDraftAssistant,
+} from "self-assert";
+import { Person } from "./Person";
+
+// #region assistant-creation
+function cretePersonAssistant() {
+  const nameAssistant = FieldDraftAssistant.handlingAll(
+    ["name.notBlank"],
+    (person: Person) => person.getName()
+  );
+
+  const ageAssistant = IntegerDraftAssistant.for(
+    "age.positive",
+    (person: Person) => person.getAge()
+  );
+
+  const personAssistant = SectionDraftAssistant.topLevelContainerWith(
+    [nameAssistant, ageAssistant],
+    (name, age) => Person.named(name, age),
+    []
+  );
+
+  return Object.assign(personAssistant, { nameAssistant, ageAssistant });
+}
+// #endregion assistant-creation
+
+// #region assistant-usage
+const personAssistant = cretePersonAssistant();
+
+personAssistant.nameAssistant.setModel("   ");
+personAssistant.ageAssistant.setModel(30);
+
+personAssistant.withCreatedModelDo(
+  () => {
+    throw new Error("this should not happen");
+  },
+  () => console.log("Person not created")
+);
+
+console.log(personAssistant.hasBrokenRules());
+
+personAssistant.nameAssistant.setModel("John");
+personAssistant.ageAssistant.setModel(50);
+
+personAssistant.withCreatedModelDo(
+  (person) => console.log(`Person created: ${person.getName()}`),
+  () => {
+    throw new Error("this should not happen");
+  }
+);
+// #endregion assistant-usage
