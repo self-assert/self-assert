dd4b9315a65da00e53af426aab295662ed872cdb
Tomás Ossa <ossatomas2@gmail.com>
2025-05-06 22:19:05 -0300

    fix: eslint and tsconfig

--- packages/core/eslint.config.js
+++ packages/core/eslint.config.js
@@ -1,13 +1,15 @@
 import tseslint from "typescript-eslint";
 import baseConfig from "@repo/support-files/eslint.config.js";
 
-export default tseslint.config({
-  ignores: [
-    "eslint.config.js",
-    "rollup.config.js",
-    "jest.config.ts",
-    "dist/**",
-    "docs/**",
-  ],
-  ...baseConfig,
-});
+export default tseslint.config(
+  {
+    ignores: [
+      "eslint.config.js",
+      "rollup.config.js",
+      "jest.config.ts",
+      "dist/**",
+      "docs/**",
+    ],
+  },
+  baseConfig
+);
