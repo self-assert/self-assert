352c77a966e688f0357d0697d0dd69649cd4dc48
Tomás Ossa <ossatomas2@gmail.com>
2025-05-13 20:08:59 -0300

    feat: create useDraftAssistantModel.ts

---
+++ packages/react/src/hooks/useDraftAssistantModel.ts
@@ -0,0 +1,33 @@
+import React from "react";
+import type { DraftAssistant, DraftViewer } from "self-assert";
+
+/**
+ * Hook to get and set the model of a `DraftAssistant`,
+ * triggering a re-render whenever the model changes.
+ */
+export function useDraftAssistantModel<Model>(
+  draftAssistant: DraftAssistant<Model>
+): [Model, (aModel: Model) => void] {
+  const [model, setModel] = React.useState(draftAssistant.getModel());
+
+  const onChange = React.useCallback(
+    (aModel: Model) => {
+      draftAssistant.setModel(aModel);
+    },
+    [draftAssistant]
+  );
+
+  React.useEffect(() => {
+    const draftViewer: DraftViewer<Model> = {
+      onDraftChanged: (aModel) => {
+        setModel(aModel);
+      },
+    };
+
+    draftAssistant.accept(draftViewer);
+
+    return () => draftAssistant.removeViewer(draftViewer);
+  }, [draftAssistant]);
+
+  return [model, onChange];
+}
