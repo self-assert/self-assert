983695b3349495c5e5e45fbf286e7352d5f8d994
Tomás Ossa <ossatomas2@gmail.com>
2025-05-16 19:59:15 -0300

    docs: add Hooks category

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,7 @@
+/**
+ * @packageDocumentation
+ * @category Hooks
+ * React hooks that act as `DraftAssistant` wrappers and trigger re-renders.
+ */
 export * from "./components";
 export * from "./hooks";
